82d70e466662ebdb62eef759bf3a262da5774fd7
eran.mazuz <eran.mazuz+git@solarwinds.com>
2023-10-25 09:10:11 +0300

    - add param to the article page.

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import {useParams} from 'react-router-dom'
 const ArticlePage = () => {
+    const { articleId} = useParams()
     return (
-        <h1>Article Page</h1>
+        <h1>Article Page with id: { articleId }</h1>
     )
 }
 
