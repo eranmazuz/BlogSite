4c8f71c3a81efb19356c7c57ca11a808eba2cc2c
eran.mazuz <eran.mazuz+git@solarwinds.com>
2023-10-25 09:32:03 +0300

    - Show the articles in the article page.

--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import articles from './article-content'
 
 const ArticlesListPage = () => {
     return (
-        <h1>Articles List Page</h1>
+        <>
+            <h1>Articles</h1>
+            { articles.map(article => (
+                <div>
+                    <h3>{ article.name }</h3>
+                    <p>{ article.content[0].substring(0,150)}...</p>
+                </div>
+            ))}
+        </>
+
     )
 }
 
