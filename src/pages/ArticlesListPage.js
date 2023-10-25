28bc825068ff01e7f57758c587426e55baebd78f
eran.mazuz <eran.mazuz+git@solarwinds.com>
2023-10-25 09:34:01 +0300

    - Link the article to the relevant article page.

--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import articles from './article-content'
+import { Link } from 'react-router-dom'
 
 const ArticlesListPage = () => {
     return (
         <>
             <h1>Articles</h1>
             { articles.map(article => (
-                <div>
+                <Link to={`/articles/${article.name}`}>
                     <h3>{ article.name }</h3>
                     <p>{ article.content[0].substring(0,150)}...</p>
-                </div>
+                </Link>
             ))}
         </>
 
